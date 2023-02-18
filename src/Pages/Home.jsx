f8e5edce10efd83606e0705c240fd7b882ab1b57
Prakash <prakashope@gmail.com>
2023-02-19 01:44:28 +0530

    displayed the state

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,24 +1,31 @@
 import axios from 'axios'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Context } from '../Context/Context'
 
 
 function Home() {
-    const {testState} = useContext(Context)
-    const [inputState , setInputState] = useState()
+  const { testState } = useContext(Context)
+  const [inputState, setInputState] = useState()
+  const [currdata , setCurrData] = useState()
+  const [data , setData] = useState([])
 
-    const shortUrlHandler = (url) => {
-          axios(
-            {
-              method: 'post',
-              url: 'http://localhost:8001/',
-              data: {"url": String(url)},
+  const shortUrlHandler = (url) => {
+    axios(
+      {
+        method: 'post',
+        url: 'http://localhost:8001/',
+        data: { "url": url }
 
-            }
-          ).then((resp) => {
-            console.log(resp)
-          })
-    }
+      }
+    ).then((resp) => {
+      data.push(resp.data)
+      setCurrData(resp.data)
+      console.log(currdata)
+      console.log(data)
+
+    })
+  }
 
 
 
@@ -28,29 +35,43 @@ function Home() {
 
       <div className='text-center text-2xl'>
 
-              Enter the URL which you want to short
+        Enter the URL which you want to short
       </div>
 
-       <div className="mt-4">
+      <div className="mt-4">
 
 
-       <input
-        className='block mx-auto outline-none text-center'
-        type="text"
-        value={inputState}
-        placeholder="Enter the URL"
-        onChange={(e) => setInputState(e.target.value)}
+        <input
+          className='block mx-auto outline-none text-center'
+          type="text"
+          value={inputState}
+          placeholder="Enter the URL"
+          onChange={(e) => setInputState(e.target.value)}
 
         ></input>
 
-       </div>
+      </div>
+
 
 
       <div className=' mt-2'>
-          <div onClick={() => shortUrlHandler(inputState)} className=' text-xl w-20 mx-auto text-center bg-gray-500 text-black'>
-              GO
-          </div>
+        <div onClick={() => shortUrlHandler(inputState)} className=' text-xl w-20 mx-auto text-center bg-gray-500 text-black'>
+          GO
+        </div>
       </div>
+
+
+      <div className='border-2 border-black'>
+
+        {data?.map((data , index) => {
+          return (
+            <div className='border-2 border-black'>
+               Shortend url for  - {data.longUrl} is <Link className='text-blue-400' to={`http://localhost:8001/${data.shortUrl}`}>http://localhost:8001/{data.shortUrl}</Link>
+            </div>
+          )
+        })}
+      </div>
+
 
     </div>
 
